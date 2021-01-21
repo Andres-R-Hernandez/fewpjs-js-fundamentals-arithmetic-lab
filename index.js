30884c2d5dd99cf5de3c3afa07cb1d2d3ebe082d
Andres-R-Hernandez <andres.hernandez@utexas.edu>
2021-01-21 15:06:55 -0600

    Done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+let multiply = num1*num2;
+
+let random =  Math.floor(Math.random() * 10) + 1;
+
+let mod = 16 % 6;
+
+let max = Math.max(1, 2, 3, 4, 5, 20);
